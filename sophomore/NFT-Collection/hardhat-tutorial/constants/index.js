da857b330c04e3be9a426f00cac407de8eb36717
Juan Pablo Vargas <jupavar@gmail.com>
2022-08-08 22:55:48 -0300

    feat: add nft-collection DAPP

---
+++ sophomore/NFT-Collection/hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x5c6Cd0ac5833B205691184193fcb8F6Ad912087a";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
