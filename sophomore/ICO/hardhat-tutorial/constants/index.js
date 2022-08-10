322ab8f914fca1ffd02464a6a9038e1972390881
Juan Pablo Vargas <jupavar@gmail.com>
2022-08-09 22:30:46 -0300

    feat: add ICO dapp

---
+++ sophomore/ICO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x6726933Eca713cAe7488b310d31191cB0aa4De1f";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
